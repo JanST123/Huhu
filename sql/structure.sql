eca08218cd515dd16ad2ad4e4a30d51ca03e66ea
jst <jst@hosteurope.de>
2014-07-09 14:18:55 +0200

    fixed sql structure

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,16 +1,23 @@
 -- phpMyAdmin SQL Dump
--- version 3.3.7deb7
+-- version 3.4.11.1deb2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Erstellungszeit: 28. August 2013 um 07:30
--- Server Version: 5.1.66
--- PHP-Version: 5.3.3-7+squeeze17
+-- Erstellungszeit: 09. Jul 2014 um 14:13
+-- Server Version: 5.5.37
+-- PHP-Version: 5.4.4-14+deb7u10
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Datenbank: `huhu`
+-- Datenbank: `deutschtalk`
 --
 
 -- --------------------------------------------------------
@@ -25,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `chats` (
   `fk_ownerUserID` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_ownerUserID` (`fk_ownerUserID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -35,36 +42,18 @@ CREATE TABLE IF NOT EXISTS `chats` (
 
 CREATE TABLE IF NOT EXISTS `chats_messages` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `message_id` varchar(24) NOT NULL,
   `fk_chatID` int(11) unsigned NOT NULL,
   `fk_userID` int(11) unsigned NOT NULL,
   `fk_recipientUserID` int(11) unsigned NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `message` longtext NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `message_id` (`message_id`,`fk_recipientUserID`),
   KEY `fk_chatID` (`fk_chatID`,`fk_userID`),
   KEY `fk_userID` (`fk_userID`),
   KEY `fk_recipientUserID` (`fk_recipientUserID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=133 ;
-
-ALTER TABLE  `chats_messages` ADD  `message_id` VARCHAR( 24 ) NOT NULL AFTER  `id` ,
-ADD UNIQUE (
-  `message_id`
-);
-
-ALTER TABLE  `huhu`.`chats_messages` DROP INDEX  `message_id` ,
-ADD UNIQUE  `message_id` (  `message_id` ,  `fk_recipientUserID` );
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `chats_messages`
---
-ALTER TABLE `chats_messages`
-  ADD CONSTRAINT `chats_messages_ibfk_1` FOREIGN KEY (`fk_chatID`) REFERENCES `chats` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `chats_messages_ibfk_2` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `chats_messages_ibfk_3` FOREIGN KEY (`fk_recipientUserID`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -75,12 +64,10 @@ ALTER TABLE `chats_messages`
 CREATE TABLE IF NOT EXISTS `chats_user` (
   `fk_chatID` int(11) unsigned NOT NULL,
   `fk_userID` int(11) unsigned NOT NULL,
-  `last_read_message_id` VARCHAR( 24 ),
+  `last_read_message_id` varchar(24) DEFAULT NULL,
   UNIQUE KEY `fk_chatID` (`fk_chatID`,`fk_userID`),
   KEY `fk_userID` (`fk_userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-ALTER TABLE  `chats_user` CHANGE  `last_read_message_id`  `last_read_message_id` VARCHAR( 24 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
-
 
 -- --------------------------------------------------------
 
@@ -97,7 +84,21 @@ CREATE TABLE IF NOT EXISTS `contactlist` (
   UNIQUE KEY `fk_ownerUserID_2` (`fk_ownerUserID`,`fk_contactUserID`),
   KEY `fk_ownerUserID` (`fk_ownerUserID`,`fk_contactUserID`,`accepted`),
   KEY `fk_contactUserID` (`fk_contactUserID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=63 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `log`
+--
+
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `priority` varchar(10) NOT NULL,
+  `message` text NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -120,36 +121,11 @@ CREATE TABLE IF NOT EXISTS `users` (
   `photo_big_height` int(5) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `invisible` tinyint(1) DEFAULT '0',
-  `key` binary(24) NOT NULL,
+  `public_key` blob,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user` (`user`),
   KEY `lastLoginTimestamp` (`lastLoginTimestamp`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=79 ;
-
-ALTER TABLE  `users` ADD  `public_key` BLOB NULL;
-ALTER TABLE `users` DROP `key`;
-
---
--- Tabellenstruktur für Tabelle `user_invisible`
---
-CREATE TABLE IF NOT EXISTS `user_invisible` (
-  `fk_userId` int(11) unsigned NOT NULL,
-  `fk_userInvisibleForId` int(11) unsigned NOT NULL,
-  UNIQUE KEY `fk_userId` (`fk_userId`,`fk_userInvisibleForId`),
-  KEY `fk_userInvisibleForId` (`fk_userInvisibleForId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Constraints der exportierten Tabellen
---
-
---
--- Constraints der Tabelle `user_invisible`
---
-ALTER TABLE `user_invisible`
-  ADD CONSTRAINT `user_invisible_ibfk_2` FOREIGN KEY (`fk_userInvisibleForId`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `user_invisible_ibfk_1` FOREIGN KEY (`fk_userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -164,7 +140,38 @@ CREATE TABLE IF NOT EXISTS `user_additional` (
   `value` varchar(200) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_userID` (`fk_userID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user_invisible`
+--
+
+CREATE TABLE IF NOT EXISTS `user_invisible` (
+  `fk_userId` int(11) unsigned NOT NULL,
+  `fk_userInvisibleForId` int(11) unsigned NOT NULL,
+  UNIQUE KEY `fk_userId` (`fk_userId`,`fk_userInvisibleForId`),
+  KEY `fk_userInvisibleForId` (`fk_userInvisibleForId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `user_push_auth`
+--
+
+CREATE TABLE IF NOT EXISTS `user_push_auth` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `fk_userID` int(11) unsigned NOT NULL,
+  `method` enum('gcm','websocket','apn') NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `valid_until` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `fk_userID_2` (`fk_userID`,`method`),
+  UNIQUE KEY `token` (`token`),
+  KEY `fk_userID` (`fk_userID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 --
 -- Constraints der exportierten Tabellen
@@ -181,7 +188,8 @@ ALTER TABLE `chats`
 --
 ALTER TABLE `chats_messages`
   ADD CONSTRAINT `chats_messages_ibfk_1` FOREIGN KEY (`fk_chatID`) REFERENCES `chats` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `chats_messages_ibfk_2` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `chats_messages_ibfk_2` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `chats_messages_ibfk_3` FOREIGN KEY (`fk_recipientUserID`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints der Tabelle `chats_user`
@@ -203,48 +211,19 @@ ALTER TABLE `contactlist`
 ALTER TABLE `user_additional`
   ADD CONSTRAINT `user_additional_ibfk_1` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
-  
-  CREATE TABLE IF NOT EXISTS `log` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `priority` varchar(10) NOT NULL,
-  `message` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4380 ;
-
-ALTER TABLE  `log` ADD  `timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
-ALTER TABLE  `users` ADD  `email` VARCHAR( 255 ) NOT NULL;
-
-
 --
--- Tabellenstruktur für Tabelle `user_push_auth`
+-- Constraints der Tabelle `user_invisible`
 --
-
-CREATE TABLE IF NOT EXISTS `user_push_auth` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `fk_userID` int(11) unsigned NOT NULL,
-  `method` enum('gcm','websocket') NOT NULL,
-  `token` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_userID` (`fk_userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- Constraints der exportierten Tabellen
---
+ALTER TABLE `user_invisible`
+  ADD CONSTRAINT `user_invisible_ibfk_1` FOREIGN KEY (`fk_userId`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_invisible_ibfk_2` FOREIGN KEY (`fk_userInvisibleForId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints der Tabelle `user_push_auth`
 --
 ALTER TABLE `user_push_auth`
   ADD CONSTRAINT `user_push_auth_ibfk_1` FOREIGN KEY (`fk_userID`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-ALTER TABLE  `user_push_auth` ADD  `valid_until` TIMESTAMP NOT NULL;
-ALTER TABLE  `user_push_auth` CHANGE  `valid_until`  `valid_until` TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
 
-ALTER TABLE  `huhu`.`user_push_auth` ADD UNIQUE (
-  `fk_userID` ,
-  `method`
-);
-ALTER TABLE  `huhu`.`user_push_auth` ADD UNIQUE (
-  `token`
-);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
